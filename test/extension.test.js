d93bd423b26e25e0177da46fc38c55c2b5f3e72f
dududuguo <dududuguo@gmail.com>
2024-06-17 16:45:07 +1000

    fix issues

--- test/extension.test.js
+++ test/extension.test.js
@@ -1,15 +1,20 @@
 const assert = require('assert');
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
+const fs = require('fs');
+const path = require('path');
 const vscode = require('vscode');
 // const myExtension = require('../extension');
 
 suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+    vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+    test('Check .ollama folder existence', async () => {
+        const homeDirectory = path.join(process.env.HOME || process.env.USERPROFILE, '.ollama');
+        const exists = fs.existsSync(homeDirectory);
+        assert.strictEqual(exists, true, 'The .ollama folder should exist in the HOME directory');
+    });
+
+    test('Sample test', () => {
+        assert.strictEqual(-1, [1, 2, 3].indexOf(5));
+        assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+    });
 });
